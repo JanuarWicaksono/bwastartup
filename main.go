645d3cbc231da8a55565d2e5c43670c8ad485e23
januar wicaksono <januarwicaksono16@gmail.com>
2020-12-15 16:56:08 +0700

    jwt fase 2

--- main.go
+++ main.go
@@ -23,6 +23,23 @@ func main() {
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
 
+	// token, err := authService.ValidateToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxfQ.jBvaWZVpqBF9oL0DTMp31fQtpJuk1D9sihuBvxz7U0c")
+	// if err != nil {
+	// 	fmt.Println("Error")
+	// 	fmt.Println("Error")
+	// 	fmt.Println("Error")
+	// }
+
+	// if token.Valid {
+	// 	fmt.Println("Valid")
+	// 	fmt.Println("Valid")
+	// 	fmt.Println("Valid")
+	// } else {
+	// 	fmt.Println("Invalid")
+	// 	fmt.Println("Invalid")
+	// 	fmt.Println("Invalid")
+	// }
+
 	userHandler := handler.NewUserHandler(userService, authService)
 
 	router := gin.Default()
@@ -34,7 +51,4 @@ func main() {
 	api.POST("/avatars", userHandler.UploadAvatar)
 
 	router.Run(":8070")
-
-	// router := gin.Default()
-	// router.Run(":8070")
 }
